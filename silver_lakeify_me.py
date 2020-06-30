3966f5ed01262917a1fc69f92b2556581d29c194
bloombar <amos@cs.nyu.edu>
2020-06-30 00:21:56 -0400

    first draft

---
+++ silver_lakeify_me.py
@@ -0,0 +1,111 @@
+"""
+Sign up for Silver Lake
+
+Selenium webdriver for Chrome (a.k.a. the file named chromedriver) must be installed in either:
+- in the same directory as chrome.exe on Windows (e.g. C:\Program Files\Google\Chrome\Application)
+- in a directory that is included in the PATH on Mac OS X (e.g. /usr/local/bin)
+"""
+
+def make_reservation(people):
+  """
+  Expects a list of people as an argument, such as:
+
+  people = [
+    {
+      'fname': 'Foo',
+      'lname': 'Barstein',
+      'phone': '6462129988',
+      'email': 'foo@barstein.com'
+    },
+    {
+      'fname': 'Bar',
+      'lname': 'Fooberger',
+      'phone': '2126468899',
+      'email': 'bar@fooberger.com'
+    },
+  ]
+
+  :param people: list of people, where each person is a dictionary with 'fname', 'lname', 'phone', and 'email' fields.
+  """
+  from selenium import webdriver
+  from selenium.webdriver import ActionChains
+  from selenium.webdriver.common.keys import Keys
+
+
+  # loop through all the people we want to make reservations for
+  for person in people:
+    # open the driver
+    url = 'https://silverlakereservations.as.me'
+    driver = webdriver.Chrome()
+    driver.get(url)
+
+    # select appointment type
+    appointment = driver.find_element_by_css_selector('#step-pick-appointment > div.pane-content > div.select.select-type > div:nth-child(4)')
+    appointment.click()
+
+    # pause for a second
+    ActionChains(driver).pause(1).perform()
+
+    # select tomorrow 11:30AM
+    # datetime = driver.find_element_by_css_selector('#dates-and-times > fieldset > div > div > div > ul > li:nth-child(1) > a')
+    # datetime.click()
+
+    # select all available options
+    num_datetimes = 0
+    datetime_options = driver.find_elements_by_css_selector('#dates-and-times > fieldset > div > div > div > ul > li > a')
+    for option in datetime_options:
+      # try to click em all! (some are not attached to page)
+      try:
+        option.click()
+        num_datetimes += 1
+      except:
+        continue
+
+    reservation_submitted = False # assume the worst, but hope for the best
+
+    # only continue if we were successfully able to click at least one datetime option
+    if num_datetimes > 0:
+      # pause for a second
+      ActionChains(driver).pause(1).perform()
+
+      # try to complete reservation
+      try:
+        # enter personal details
+        fname = driver.find_element_by_css_selector('#first-name')
+        fname.send_keys(person['fname'])
+        lname = driver.find_element_by_css_selector('#last-name')
+        lname.send_keys(person['lname'])
+        phone = driver.find_element_by_css_selector('#phone')
+        phone.send_keys(person['phone'])
+        email = driver.find_element_by_css_selector('#email')
+        email.send_keys(person['email'])
+
+        # pause for a second
+        ActionChains(driver).pause(1).perform()
+
+        # submit the reservation
+        submit = driver.find_element_by_css_selector('#custom-forms > div > div > input')
+        submit.click()
+
+        reservation_submitted = True
+
+      except:
+        pass
+
+    if reservation_submitted:
+      # log it
+      print('submitted {} reservations for {} {}'.format(num_datetimes, person['fname'], person['lname']))
+    else:
+      # log it
+      print('no reservation available for {} {}'.format(person['fname'], person['lname']))
+
+    # finished!
+    driver.close()
+
+
+# mouse over
+# ActionChains(driver).move_to_element(trigger).perform() 
+
+# pause for a second
+# ActionChains(driver).pause(1).perform()
+
